9ac87333f0568265e044d16330d825a6972a4153
chadcoulter <69040524+chadcoulter@users.noreply.github.com>
2026-08-20 21:54:51 -0500

    feat: register model and mutation commands

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -1,16 +1,23 @@
 from evennia import default_cmds
 
+from .model_api import CmdModelMove, CmdModelObserve, CmdModelSay
 from .room_admin import CmdAdminStatus, CmdReleaseAdmin, CmdRequestAdmin
+from .room_mutation import CmdAdminDescribe, CmdAdminOpen
 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
-    """Default character commands plus arena authority commands."""
+    """Default character commands plus arena actor and authority commands."""
 
     def at_cmdset_creation(self):
         super().at_cmdset_creation()
         self.add(CmdRequestAdmin())
         self.add(CmdReleaseAdmin())
         self.add(CmdAdminStatus())
+        self.add(CmdAdminDescribe())
+        self.add(CmdAdminOpen())
+        self.add(CmdModelObserve())
+        self.add(CmdModelSay())
+        self.add(CmdModelMove())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
