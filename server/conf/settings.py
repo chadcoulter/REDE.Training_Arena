605d66911d66839e3be04747aeea3451060fb0cf
chadcoulter <69040524+chadcoulter@users.noreply.github.com>
2026-08-20 21:45:59 -0500

    feat: add Evennia arena settings

---
+++ server/conf/settings.py
@@ -0,0 +1,11 @@
+from evennia.settings_default import *
+
+SERVERNAME = "REDE.Training_Arena"
+
+BASE_ROOM_TYPECLASS = "typeclasses.rooms.Room"
+BASE_CHARACTER_TYPECLASS = "typeclasses.characters.Character"
+
+CMDSET_CHARACTER = "commands.default_cmdsets.CharacterCmdSet"
+CMDSET_SESSION = "commands.default_cmdsets.SessionCmdSet"
+CMDSET_ACCOUNT = "commands.default_cmdsets.AccountCmdSet"
+CMDSET_UNLOGGEDIN = "commands.default_cmdsets.UnloggedinCmdSet"
